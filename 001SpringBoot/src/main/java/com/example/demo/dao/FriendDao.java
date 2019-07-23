5f5344981da7573e0433c8bf83721f3d061d3f4d
Prafful Daga <prraful@gmail.com>
2019-07-23 16:37:53 +0530

    spring boot get all friends - first day of spring reactive

---
+++ 001SpringBoot/src/main/java/com/example/demo/dao/FriendDao.java
@@ -0,0 +1,22 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.FriendEntity;
+import com.example.demo.repository.FriendRepository;
+
+@Repository("friendDao")
+public class FriendDao {
+
+	@Autowired
+	private FriendRepository friendRepository;
+	
+	public List<FriendEntity> getAllFriends() {
+		
+		return (List<FriendEntity>) friendRepository.findAll();		
+	}
+
+}
